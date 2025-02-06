8b7050a1d36ca99c963a716621bd9f26946d6c2f
Andrew Irorere <andreyscott301@gmail.com>
2025-02-06 13:24:39 +0100

    starter file with andriod and ios permission enabled

---
+++ android/app/src/main/kotlin/com/example/imagetotext/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.imagetotext
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/claude_service.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+class ClaudeService {
+  final String _baseUrl = 'https://api.anthropics.com/v1/messages';
+  final String _apiKey = 'c2c7b3b0-7b7b-11e9-8f9e-2a86e4085a59';
+
+  Future<String> analyze(File image) async {
+    final bytes = await image.readAsBytes();
+    final base64Image = base64Encode(bytes);
+
+    final response = await http.post(
+      Uri.parse(_baseUrl),
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': _apiKey,
+        'anthropic-version': '2023-06-01',
+      },
+      body: jsonEncode({
+        'model': 'claude-3-opus-20240229',
+        'max_tokens': 180,
+        'message': [
+          {
+            'role': 'user',
+            'content': [
+              {
+                'type': 'image',
+                'source': {
+                  'type': 'base64',
+                  'media_type': 'image/jpeg',
+                  'data': base64Image,
+                },
+              },
+              {
+                'type': 'text',
+                'text': 'Please describe what you see in this image.',
+              }
+            ],
+          }
+        ]
+      }),
+    );
+    // if successful
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      return data['content'][0]['text'];
+    } else {
+      throw Exception('Failed to analyze image: ${response.statusCode}');
+    }
+  }
+}
